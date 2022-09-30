d3d2159787c9890c21b513e3ed5cd7da317e570d
Yeolyi <yeolyi1310@gmail.com>
2022-10-01 01:04:09 +0900

    cors 허용 추가

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express = require('express');
+import cors = require('cors');
 import { convertAll } from './convert';
 
 const app = express();
+app.use(cors());
+
 convertAll();
 app.use(express.static('converted'));
 app.listen(3000);
