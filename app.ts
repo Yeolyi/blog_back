c1b8caec21277eb4f1219f597b08d87b31989b38
Yeolyi <yeolyi1310@gmail.com>
2022-09-30 17:43:59 +0900

    static serving 추가

--- app.ts
+++ app.ts
@@ -1,11 +1,9 @@
-import express = require("express");
+import express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(express.static('blog_src'));
 
 app.listen(port, () => {
-  console.log(`Exmaple app lsitening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
