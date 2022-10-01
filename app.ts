ad200ff53f0aeec964bd60fb481cf8e1f2fe6639
Yeolyi <yeolyi1310@gmail.com>
2022-10-01 15:17:21 +0900

    .env 추가

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import express = require('express');
 import cors = require('cors');
 import { convertAll } from './convert';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
 convertAll();
 app.use(express.static('converted'));
-app.listen(3000);
+app.listen(process.env.PORT);
+console.log(process.env.PORT);
