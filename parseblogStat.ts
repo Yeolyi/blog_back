dbbe372e1e78f98e74df03bfa57e3d844f332859
Yeolyi <yeolyi1310@gmail.com>
2022-11-08 19:54:14 +0900

    blogStat 색정보 타입 수정

--- parseblogStat.ts
+++ parseblogStat.ts
@@ -12,7 +12,7 @@ const parseBlogStat = async () => {
   };
   const git = simpleGit(options);
 
-  const { all: commits } = await git.log(['-10']);
+  const { all: commits } = await git.log();
 
   const commitStatsPromise = [...Array(commits.length - 1).keys()].map(
     async (idx) => {
@@ -23,10 +23,13 @@ const parseBlogStat = async () => {
         previousCommit.hash,
         baseCommit.hash,
       ]);
+
       const lines = diff.split('\n');
       let added = 0;
-      let deleted = 0;
-      const colorMap: { [color: string]: number } = {};
+
+      const colorMap: {
+        [ext: string]: { color: string; language: string; colorAmount: number };
+      } = {};
 
       lines.forEach((line) => {
         const regex = /(\d+)\s+(\d+)\s+(.+)/;
@@ -42,7 +45,6 @@ const parseBlogStat = async () => {
         }
 
         added += +match[1];
-        deleted += +match[2];
 
         const extTemp = match[3].split('.');
         const ext = extTemp[extTemp.length - 1];
@@ -53,32 +55,37 @@ const parseBlogStat = async () => {
         }
 
         const color = getLanguageColor(language);
-        colorMap[color] = (colorMap[color] ?? 0) + +match[1];
+        colorMap[ext] = {
+          language,
+          color,
+          colorAmount: colorMap[ext]?.colorAmount ?? 0 + added,
+        };
       });
 
       if (added === 0) {
         return null;
       }
 
-      const colorMapMax = Object.values(colorMap).reduce((a, b) => a + b, 0);
-      const temp = Object.entries(colorMap)
-        .sort((a, b) => b[1] - a[1])
-        .reduce<[string, number][]>(
-          (prev, cur) => [...prev, [cur[0], cur[1] / colorMapMax]],
-          []
-        );
+      const colorMapMax = Object.values(colorMap).reduce(
+        (a, b) => a + b.colorAmount,
+        0
+      );
+      const temp = Object.values(colorMap)
+        .sort((a, b) => (a.language < b.language ? -1 : 1))
+        .map((x) => ({ ...x, colorAmount: x.colorAmount / colorMapMax }));
 
       return {
         message: baseCommit.message,
         added,
-        deleted,
-        description: diff,
         colors: temp,
+        date: baseCommit.date,
       };
     }
   );
 
-  return (await Promise.all(commitStatsPromise)).filter((x) => x !== null);
+  return (await Promise.all(commitStatsPromise))
+    .filter((x) => x !== null)
+    .slice(0, 10);
 };
 
 export default parseBlogStat;
