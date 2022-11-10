8e54da1013534b13fa203522e84e0fa61b2b9398
Yeolyi <yeolyi1310@gmail.com>
2022-11-11 00:42:16 +0900

    build 폴더를 계산에 포함

--- lib/isAllowedFilePath.ts
+++ lib/isAllowedFilePath.ts
@@ -1,7 +1,6 @@
 export const isAllowedFilePath = (filePath: string) => {
   return (
     !filePath.endsWith('package-lock.json') &&
-    !filePath.endsWith('package.json') &&
-    !filePath.includes('/build/')
+    !filePath.endsWith('package.json')
   );
 };
