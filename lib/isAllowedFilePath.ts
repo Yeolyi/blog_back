2f2c31dbbc711dfa799c2fd4f50d79a53da4f0d2
Yeolyi <yeolyi1310@gmail.com>
2022-11-17 00:11:57 +0900

    커밋 계산 예외에 bundle.js 추가

--- lib/isAllowedFilePath.ts
+++ lib/isAllowedFilePath.ts
@@ -1,6 +1,7 @@
 export const isAllowedFilePath = (filePath: string) => {
   return (
     !filePath.endsWith('package-lock.json') &&
-    !filePath.endsWith('package.json')
+    !filePath.endsWith('package.json') &&
+    !filePath.endsWith('bundle.json')
   );
 };
